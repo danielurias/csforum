6c452b57d4abc467b6bfd897a99aec6df6318693
Daniel Urias <danielurias@Daniels-MacBook-Pro.local>
2021-07-06 12:19:08 -0600

    update server

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ module.exports = server;
 
 // GET /thread
 
+
 // this is where we will do our own middleware
 
 server.get('/thread', (req, res) => {
@@ -45,10 +46,10 @@ server.get('/thread', (req, res) => {
 
 // GET /thread/:id
 
-server.get('/thread/:id', (req, res) => {
+server.get('/thread/:threadId', (req, res) => {
     res.setHeader("Content-Type", "applicaiton/json" );
-    console.log(`Getting thread with id ${req.params}`);
-    model.Thread.findById(req.params.id, (error, thread) => {
+    console.log(`Getting thread with id ${req.params.id}`);
+    model.Thread.findById(req.params.threadId, (error, thread) => {
         if (err != null) {
             res.status(500).json({
                 error: err,
@@ -60,16 +61,45 @@ server.get('/thread/:id', (req, res) => {
                 error: err,
                 message: "Could not list thread error 404"
             });
+            return;
         }
+        res.status(200).json(thread);
     });
 });
 
 // POST /thread
-
+server.post('/thread', (req, res) => {
+    res.setHeader("Content-Type", "applicaiton/json" );
+    var thread = new model.Thread({
+      author: req.body.author,
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+    });
+  
+    thread.save().then((thread) => {
+      console.log('Thread created');
+      res.status(201).json(thread);
+    }).catch(function (err) {
+        if (err != null) {
+            res.status(500).json({
+                error: err,
+                message: "Could not post Post"
+            });
+            return;
+        } 
+        res.status(201).json(thread);
+    });
+  });
 
 // DELETE /thread/:id
 
+server.delete("/thread/:id", (req, res) => {
+    res.setHeader("Content-Type", "applicaiton/json" );
+})
+
 // POST /post
+
 server.post('/post', (req, res) => {
     res.setHeader("Content-Type", "applicaiton/json" );
     console.log("Posting thread", req.body);
@@ -85,28 +115,33 @@ server.post('/post', (req, res) => {
         {
             $push: {posts: newPost},
         },
-        (err, threads) => {
+        {
+            new: true,
+        },
+        (err, thread) => {
         if (err != null) {
             res.status(500).json({
                 error: err,
                 message: "Could not post Post"
             });
             return;
-        } else if (err === null) {
+        } else if (thread === null) {
             res.status(404).json({
                 error: err,
                 message: "Could not post error 404"
             });
+            return;
         }
+        res.status(200).json(thread.posts[threads.post.length - 1]);
     });
 });
 
 // DELETE /post/:thread_id/:post_id
 
-server.delete("/post/:id", (req, res) => {
+server.delete("/post/:thread_id/:post_id", (req, res) => {
     res.setHeader("Content-Type", "applicaiton/json");
     console.log(
-        `deleting post with id ${req.params.thread_id}`
+        `deleting post with id ${req.params.post_id} on thread with id ${req.params.thread_id}`
     );
     model.Thread.findByIdAndUpdate(
         req.params.thead_id,
@@ -132,7 +167,24 @@ server.delete("/post/:id", (req, res) => {
                     error: err,
                     message: "Could not list thread error 404"
                 });
+                return;
             }
+
+            let post;
+            thead.posts.forEach((e) => {
+                if (e._id == req.params.post_id) {
+                    post = e;
+                }
+            });
+            
+            if (post == undefined) {
+                res.status(404).json({
+                    error: err,
+                    message: "could not find post",
+                });
+                return;
+            }
+            res.status(200).json(post);
         }
     );
 });
